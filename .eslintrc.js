3e85fc96dbc0fb28ed47daf79d7e642590f3564a
xie <xieqiang9408@gmail.com>
2018-09-18 22:48:38 +0800

    update:lint style

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,20 +3,20 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/strongly-recommended", "@vue/prettier"],
+  extends: ['plugin:vue/recommended', '@vue/standard'],
   rules: {
-    "no-console": "off",
-    "no-debugger": "off",
-    "vue/attribute-hyphenation": ["error", "always"],
-    "vue/html-indent": ["error", 2],
-    "vue/html-self-closing": "error",
-    "vue/require-default-prop": "error",
-    "vue/require-prop-types": "error",
-    "vue/attributes-order": "error",
-    "vue/html-quotes": ["error", "double"],
-    "vue/order-in-components": "error"
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'vue/attribute-hyphenation': ['error', 'always'],
+    'vue/html-indent': ['error', 2],
+    'vue/html-self-closing': 'error',
+    'vue/require-default-prop': 'error',
+    'vue/require-prop-types': 'error',
+    'vue/attributes-order': 'error',
+    'vue/html-quotes': ['error', 'double'],
+    'vue/order-in-components': 'error'
   },
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint'
   }
-};
+}
